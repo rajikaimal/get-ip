0d8156e969317f0ac772529aabf372a62a4c4df8
rajikaimal <inbox.rajika@gmail.com>
2016-11-21 21:50:12 +0530

    init

---
+++ test/index.js
@@ -0,0 +1,20 @@
+const getIp = require('../index');
+const assert = require('chai').assert;
+const isIp = require('is-ip');
+
+describe('test get-ip', function() {
+  it('should return an IP address', function(done) {
+    const req = {};
+    Object.defineProperty(req, 'connection', {
+      value: {}
+    });
+    Object.defineProperty(req.connection, 'remoteAddress', {
+      value: '172.168.10.5'
+    });
+    const ip = getIp(req);
+    if(isIp(ip)) {
+        assert('172.168.10.5', ip);
+        done();
+    }
+  });
+});
